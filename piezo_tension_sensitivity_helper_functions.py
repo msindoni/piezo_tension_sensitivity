6f1eb2b06ecfc6f91cf6a6fea98946a93d635182
Michael Sindoni <mjs164@dhe.duke.edu>
2026-01-06 12:11:07 -0500

    pressure analysis update

--- piezo_tension_sensitivity_helper_functions.py
+++ piezo_tension_sensitivity_helper_functions.py
@@ -14,6 +14,385 @@ from plotly.subplots import make_subplots
 from ipywidgets import widgets
 from IPython.display import display
 
+
+
+################################## DIC MICROSCOPY IMAGE DATA ANALYSIS ########################################################
+def circle_fit(x, r, h, k):
+    '''This function calculates the y-coordinate of a circle given the x-coordinate, radius, and center coordinates.
+
+    Inputs:
+        x: The x-coordinate(s) at which to calculate the y-coordinate(s) of the circle.
+        r: The radius of the circle.
+        h: The x-coordinate of the center of the circle.
+        k: The y-coordinate of the center of the circle.
+
+    Outputs:
+        The y-coordinate(s) of the circle at the given x-coordinate(s).'''
+    return k + np.sqrt(r**2 - (x - h)**2)
+
+def get_cropped_image(file_path_name):
+    '''This function opens an image, allows the user to select a region of interest (ROI) using a bounding box, 
+    crops the image based on the ROI, and returns the cropped image, cropped coordinates, and the original image.
+
+    Inputs:
+        file_path_name: The path and filename of the image to be processed.
+
+    Outputs:
+        roi_cropped: The cropped image based on the selected ROI.
+        cropped_coordinates: The x, y, width, and height of the selected ROI.
+        img_raw: The original image.'''
+    
+    #open image, get coordinates for the x, y (top left corner), w (width to the right), and h (hight down)
+    # Naming a window
+    cv2.namedWindow("Resized_Window", cv2.WINDOW_NORMAL)
+
+    # Using resizeWindow()
+    cv2.resizeWindow("Resized_Window", 900, 900)
+    img_raw = cv2.imread(file_path_name)[:, :, 0]
+    x, y, w, h = cv2.selectROI('Resized_Window', img_raw)
+    cropped_coordinates = [x, y, w, h]
+    cv2.destroyAllWindows()
+
+    #crop image based on x, y, w, h coordinates and show cropped section
+    roi_cropped = img_raw[int(y):int(y+h),int(x):int(x+w)]
+    sns.heatmap(roi_cropped, cmap = 'Greys_r', cbar = False)
+    plt.show()
+
+    return roi_cropped, cropped_coordinates, img_raw
+
+def find_membrane(roi_cropped):
+    '''This function takes a cropped image as input, processes the image to find the coordinates of the darkest pixel 
+    in each column (representing the membrane), and returns a dataframe containing the x and y coordinates of the membrane.
+
+    Inputs:
+        roi_cropped: The cropped image.
+
+    Outputs:
+        membrane_df: A dataframe containing the x and y coordinates of the membrane.'''
+
+    #making an array of repeating large number. This way later argmin won't pick up zeros like it would with np.zero. 1000000 is arbitrary
+    #really need to find a clwaner way to do this
+    avgpix_array = np.full((roi_cropped.shape[0], roi_cropped.shape[1]), 1000000)
+
+    #go through each colum and find the average value for each chunk of five pixels
+    #Add that value to the blank zero array in the center position of each finve pixel chunk
+    for i in range(roi_cropped.shape[1]):
+        pix_start = 0
+        while pix_start + 3 <= roi_cropped.shape[0]:
+            pix_to_avg = np.average(roi_cropped[pix_start:pix_start+3, i])
+            pix_start += 3
+            avgpix_array[pix_start-2, i] = pix_to_avg
+
+    #find darkest pixel (center pixel of 5 pixel chunk) for each column and generate x coordinates to graph with, convert to dataframe
+    y_coordinates = np.argmin(avgpix_array, axis = 0)
+    x_coordinates = np.linspace(0, roi_cropped.shape[1]-1, roi_cropped.shape[1], dtype=int)
+    mem_coordinates = {'x_coordinates':x_coordinates, 'y_coordinates':y_coordinates}
+    membrane_df = pd.DataFrame(mem_coordinates)
+
+    #plot and check to make sure overall the membrane is selected
+    plt.scatter(membrane_df['x_coordinates'], membrane_df['y_coordinates'], s = 4, color ='red')
+    plt.plot(membrane_df['x_coordinates'], membrane_df['y_coordinates'], color = 'gold')
+    plt.imshow(roi_cropped)
+    plt.show()
+
+    return membrane_df
+
+def fix_mem_loc(df):
+    '''This function takes a dataframe containing x and y coordinates of a membrane, allows the user to specify start and end points
+    for the membrane, and optionally removes problem points. It returns a filtered dataframe with the selected membrane coordinates.
+
+    Inputs:
+      df: The dataframe containing x and y coordinates of the membrane.
+
+    Outputs:
+      df_fixed_mem_loc: The filtered dataframe with selected membrane coordinates.
+
+    '''
+
+    # Create a scatter plot of the raw membrane location
+    raw_mem_loc_fig = go.Figure(go.Scatter(x=df['x_coordinates'], y=df['y_coordinates']))
+    raw_mem_loc_fig.show()
+
+    # Prompt the user to input the start and end points of the membrane
+    front_cut = input('Membrane start: ')
+    end_cut = input('Membrane end: ')
+
+    # Prompt the user to input problem points (x values) to remove, separated by spaces
+    problem_points = input('Problem points (x val) / n: ').split()
+
+    # Filter the dataframe based on the specified start and end points
+    df_fixed_mem_loc = df.loc[(df['x_coordinates'] > int(front_cut)) & (df['x_coordinates'] < int(end_cut))]
+
+    # Remove problem points from the dataframe, if specified by the user
+    if problem_points[0] != 'n':
+        for i in range(len(problem_points)):
+            df_fixed_mem_loc = df_fixed_mem_loc[df_fixed_mem_loc['x_coordinates'] != int(problem_points[i])]
+
+        # Create a scatter plot of the fixed membrane location
+        fixed_mem_loc_fig = go.Figure(go.Scatter(x=df_fixed_mem_loc['x_coordinates'], y=df_fixed_mem_loc['y_coordinates']))
+        fixed_mem_loc_fig.show()
+    else:
+        # Create a scatter plot of the fixed membrane location
+        fixed_mem_loc_fig = go.Figure(go.Scatter(x=df_fixed_mem_loc['x_coordinates'], y=df_fixed_mem_loc['y_coordinates']))
+        fixed_mem_loc_fig.show()
+
+    return df_fixed_mem_loc
+
+def _1gaussian(x, amp1,cen1,sigma1):
+    '''This function calculates the value of a Gaussian distribution at a given point.
+
+    Inputs:
+    x: The point at which to evaluate the Gaussian distribution.
+    amp1: The amplitude of the Gaussian curve.
+    cen1: The center (mean) of the Gaussian curve.
+    sigma1: The standard deviation of the Gaussian curve.
+
+    Outputs:
+    The calculated value of the Gaussian distribution at the given point `x`.'''
+
+    return amp1*(1/(sigma1*(np.sqrt(2*np.pi))))*(np.exp((-1.0/2.0)*(((x-cen1)/sigma1)**2)))
+
+def gaus_find_membrane(mem_loc_fixed, roi_cropped):
+    '''This function performs Gaussian fitting on the membrane coordinates and the cropped image. It calculates the Gaussian 
+    position and sigma for each x-coordinate of the membrane.
+
+    Inputs:
+      mem_loc_fixed: The dataframe containing fixed membrane coordinates.
+      roi_cropped: The cropped image.
+
+    Outputs:
+      df_gaus_data: A dataframe containing the Gaussian positions and sigmas for each x-coordinate of the membrane.
+      fig: The plotted figure showing the membrane coordinates and the cropped image.'''
+
+    mem_gaus_position_list = []
+    sigma_list = []
+    
+    # Iterate over each x-coordinate in the fixed membrane coordinates
+    for i in mem_loc_fixed['x_coordinates']:
+        midpoint = mem_loc_fixed.loc[i][1]
+        
+        # Generate a list of 1D point locations for fitting
+        point_location_1d_list_prefix = np.linspace(midpoint - 10, midpoint + 10, 21)
+        shift_value = min(point_location_1d_list_prefix)
+        point_location_1d_list_adjusted = point_location_1d_list_prefix - shift_value
+
+        # Extract the corresponding pixel values from the cropped image
+        pixel_val_list_prefix = roi_cropped[int(min(point_location_1d_list_prefix)):int(max(point_location_1d_list_prefix)) + 1, mem_loc_fixed.loc[i][0]] * -1
+        pixel_val_list_adjusted = pixel_val_list_prefix + (min(pixel_val_list_prefix) * -1)
+
+        # Perform curve fitting using the Gaussian function
+        popt, pcov = curve_fit(_1gaussian, point_location_1d_list_adjusted, pixel_val_list_adjusted)
+
+        # Calculate the Gaussian position by adding the shift value
+        mem_gaus_point = popt[1] + shift_value
+        mem_gaus_position_list.append(mem_gaus_point)
+        sigma_list.append(popt[2])
+
+    # Create a dataframe containing the Gaussian positions and sigmas
+    df_gaus_data = pd.DataFrame({'gaus_x': mem_loc_fixed['x_coordinates'],
+                                'gaus_y': mem_gaus_position_list,
+                                'sigma': sigma_list})
+
+    # Plot the membrane coordinates and the cropped image
+    fig = plt.figure(figsize=(3, 2), dpi=250)
+    plt.scatter(mem_loc_fixed['x_coordinates'], mem_gaus_position_list, s=10, label='data', color='red', edgecolor='black', linewidth=0.5)
+    plt.imshow(roi_cropped)
+
+    return df_gaus_data, fig
+
+def remove_low_gaus_sigma(df_gaus_data, roi_cropped):
+    '''This function removes data points from df_gaus_data based on a threshold for the sigma value. It creates a new dataframe 
+    with the points that have a sigma greater than 1.
+
+    Inputs:
+      df_gaus_data: The dataframe containing Gaussian positions and sigmas.
+      roi_cropped: The cropped image.
+
+    Outputs:
+      df_gaus_data_cutsigma: The new dataframe with points that have a sigma greater than 1.'''
+
+    #make a new dataframe with these points removed
+    df_gaus_data_cutsigma=df_gaus_data.loc[df_gaus_data['sigma']>1]
+    #getting the cut points to graph
+    df_gaus_data_highsigma=df_gaus_data.loc[df_gaus_data['sigma']<1]
+    #make figure to show cut points
+    fig, ax = plt.subplots(figsize=(8, 5))
+    ax.scatter(df_gaus_data_cutsigma['gaus_x'], df_gaus_data_cutsigma['gaus_y'],  s=100, label='data',
+               color = 'red', edgecolor='black', linewidth=1)
+    ax.scatter(df_gaus_data_highsigma['gaus_x'], df_gaus_data_highsigma['gaus_y'], s=100, label='data',
+                color = 'gold', edgecolor='black', linewidth=1)
+    ax.imshow(roi_cropped)
+    
+    return df_gaus_data_cutsigma
+
+def fix_mem_loc_gaus(df_gaus_data):
+    '''This function creates a scatter plot of the Gaussian positions in df_gaus_data and allows for the removal of specific 
+    points based on user input.
+
+    Inputs:
+      df_gaus_data: The dataframe containing Gaussian positions.
+
+    Outputs:
+      df_gaus_data: The modified dataframe after removing specific points.'''
+
+    # Create a scatter plot of the raw Gaussian positions
+    raw_mem_loc_fig = go.Figure(go.Scatter(x=df_gaus_data['gaus_x'], y=df_gaus_data['gaus_y']))
+    raw_mem_loc_fig.show()
+
+    problem_points = input('Problem points (x val) / n: ').split()
+
+    # Remove specific points based on user input
+    if problem_points[0] != 'n':
+        for i in range(len(problem_points)):
+            df_gaus_data = df_gaus_data[df_gaus_data['gaus_x'] != int(problem_points[i])]
+
+    # Create a scatter plot of the fixed Gaussian positions
+    fixed_mem_loc_fig = go.Figure(go.Scatter(x=df_gaus_data['gaus_x'], y=df_gaus_data['gaus_y']))
+    fixed_mem_loc_fig.show()
+
+    return df_gaus_data
+
+def fit_circle_gaus_weighted(circle_fit, df_gaus_data, roi_cropped):
+    '''This function fits a circle to the Gaussian positions in df_gaus_data using a weighted standard deviation approach.
+
+    Inputs:
+      circle_fit: The circle fitting function.
+      df_gaus_data: The dataframe containing Gaussian positions and sigmas.
+      roi_cropped: The cropped image.
+
+    Outputs:
+      popt: The optimized parameters of the circle fit.
+      fit_stdvs: The standard deviations of the fitted parameters.
+    '''
+
+    # Guess initial values for the circle fit parameters
+    r_guess = 100
+    h_guess = df_gaus_data['gaus_x'].median()
+    k_guess = 0
+    guess_para = [r_guess, h_guess, k_guess]
+
+    # Perform curve fitting with weighted standard deviation
+    popt, pcov = curve_fit(circle_fit, df_gaus_data['gaus_x'], df_gaus_data['gaus_y'], guess_para, maxfev=5000, sigma=df_gaus_data['sigma'])
+
+    # Extract variances and convert to standard deviations for each fit parameter
+    pcov_diag = np.diag(pcov).tolist()
+    radius_std = pcov_diag[0]
+    h_std = pcov_diag[1]
+    k_stdv = pcov_diag[2]
+    fit_stdvs = [radius_std, h_std, k_stdv]
+
+    # Plot the circular fit along with the original heatmap, scatter of points used, and line graph of connected dots
+    fitline = np.linspace(0, roi_cropped.shape[1] - 1, num=1000)
+    plt.plot(fitline, circle_fit(fitline, *popt), color='gold', linewidth=4, linestyle='dashed', dashes=(1, 1))
+    plt.imshow(roi_cropped)
+    plt.plot(df_gaus_data['gaus_x'], df_gaus_data['gaus_y'], color='red')
+    plt.show()
+
+    return popt, fit_stdvs
+
+def combined_image_traces(popt, cropped_coordinates, mem_loc_df, img_raw, pressure_step):
+    '''This function generates a combined plot of the fitted circle on the original image and calculates the radius and tension of the membrane.
+
+    Inputs:
+      popt: The optimized parameters of the circle fit.
+      cropped_coordinates: The coordinates of the cropped region (x, y, width, height).
+      mem_loc_df: The dataframe containing membrane location data.
+      img_raw: The original image.
+      pressure_step: The pressure step value.
+
+    Outputs:
+      fig: The figure object of the combined plot.
+      tension: The calculated tension of the membrane.
+      true_r: The calculated radius of the membrane.'''
+    
+    x, y, h, w = cropped_coordinates
+    r, xpoint, ypoint = popt
+
+    #plotting fit onto the original image
+    fitline = np.linspace(x-r, x+w+r, num = 1000)
+
+    fig = plt.figure(figsize=(6, 4), dpi=300)
+
+    plt.plot(fitline, circle_fit(fitline, r, xpoint+x, ypoint+y), color = 'red', linewidth = 2, linestyle='dashed', dashes=(0.5, 1))
+    plt.imshow(img_raw, cmap='gray')
+    plt.show()
+
+
+    true_r = r * 0.03225 * 10**-6
+    #mmHg to F/m^2 conversion factor = 133.322
+    tension = ((true_r * (pressure_step * 133.322)))/2*10**3
+    print('Radius', round(true_r*10**6, 3),('um'))
+    print('Tension', round(tension, 3), 'mN/m')
+
+    return fig, round(tension,3), round(true_r*10**6, 3)
+
+def update_tension_pressure(pressure_tension, tension, pressure_step, circle_fit_data, fit_stdvs):
+    '''This function updates the pressure_tension dictionary with the calculated tension, circle fit data, and fit standard deviations for a specific pressure step.
+
+    Inputs:
+      pressure_tension: The dictionary storing tension and fit data for each pressure step.
+      tension: The calculated tension for the current pressure step.
+      pressure_step: The current pressure step.
+      circle_fit_data: The optimized parameters and fit data from the circle fit.
+      fit_stdvs: The standard deviations of the fit parameters.
+
+    Outputs:
+      pressure_tension: The updated pressure_tension dictionary.'''
+
+    # Update the pressure_tension dictionary with the calculated values
+    pressure_tension[pressure_step] = [tension, round(circle_fit_data[0] * 0.03225, 3), round(np.sqrt(fit_stdvs[0]) * 0.03225, 3), round(np.sqrt(fit_stdvs[1]) * 0.03225, 3), round(np.sqrt(fit_stdvs[2]) * 0.03225, 3)]
+
+    # Print the updated pressure_tension dictionary
+    print(pressure_tension)
+
+    return pressure_tension
+
+def generate_final_tension_df(updated_pressure_tension_dict):
+    '''This function generates a final DataFrame containing tension, radius, pressure, radius standard deviation,
+    h standard deviation, k standard deviation, and percent standard error values.
+
+    Inputs:
+      updated_pressure_tension_dict: The updated pressure_tension dictionary.
+
+    Outputs:
+      df_tension_final: The final DataFrame with tension and fit data.'''
+
+    # Initialize empty lists to store the data
+    rad_list = []
+    tension_list = []
+    pressure_list = list(updated_pressure_tension_dict)
+    radius_stdv_list = []
+    h_stdv_list = []
+    k_stdv_list = []
+
+    # Iterate over the updated_pressure_tension_dict and extract the data
+    for i in updated_pressure_tension_dict:
+        current_list = updated_pressure_tension_dict[i]
+        tension_list.append(current_list[0])
+        rad_list.append(current_list[1])
+        radius_stdv_list.append(current_list[2])
+        h_stdv_list.append(current_list[3])
+        k_stdv_list.append(current_list[4])
+
+    # Calculate the percent standard error
+    percent_std_error_list = [(x/y)*100 for x,y in zip(radius_stdv_list, rad_list)]
+
+    # Create the final DataFrame
+    df = pd.DataFrame({'pressure': pressure_list,
+                       'rad': rad_list,
+                       'tension': tension_list,
+                       'radius_stdv': radius_stdv_list,
+                       'h_stdv': h_stdv_list,
+                       'k_stdv': k_stdv_list,
+                       'percent_std_error': percent_std_error_list})
+
+    # Sort the DataFrame by pressure
+    df_tension_final = df.sort_values(by=['pressure'])
+
+    return df_tension_final
+
+
+
 ################################## STRETCH EPHYS DATA ACQUISITION ########################################################
 
 def load_dataframe(file):
@@ -509,6 +888,70 @@ def steady_state_currents(df_ephys, df_summary):
     df_summary['ssc_norm']=ssc_norm_list
     
     return df_summary
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 ################################## STRETCH EPHYS DATA ANALYSIS ########################################################
 def generate_p50_multiple(df_piezos):
